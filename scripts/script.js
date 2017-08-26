6fb76d906713a518180c5ba4577875dea41f5ea6
Naomi <naomi.idowu@gmail.com>
2017-08-26 00:33:39 -0500

    first c

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+var screen = document.getElementById('screen');
+var btn = document.getElementsByClassName('btn');
+ var one = document.getElementById('one');
+var two = document.getElementById('two');
+var three = document.getElementById('three');
+
+btn[0].addEventListener('click', function(event){
+
+  screen.value = 1;
+
+});
